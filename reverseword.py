f680daaf2eea360b5be937febca26ec560eb65f1
acds-LosHumphrey <36047116+acds-LosHumphrey@users.noreply.github.com>
2018-04-19 17:48:44 -0500

    Add files via upload

--- reverseword.py
+++ reverseword.py
@@ -1,21 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """@package docstring
 Reverse Word.
 
 Receives as a paramenter one sentence. As a result, it prints the sentence in a reverse order.
 """
+
+
 def reverse(userInput):
-	"""Function Reverse.
-	Receives userInput as a sentence
+    """Function Reverse.
+    Receives userInput as a sentence
     """
-    userInputArray = userInput.split(" ")
-    wordResult = ""
-    for i in range(1, len(userInputArray)+1):
-    	"""Iterator to go over the sentence and put it backwards"""
-        wordResult += userInputArray[len(userInputArray)-i]
+
+    userInputArray = userInput.split(' ')
+    wordResult = ''
+    for i in range(1, len(userInputArray) + 1):
+        wordResult += userInputArray[len(userInputArray) - i]
         if i < len(userInputArray):
-        	wordResult += " "
+            wordResult += ' '
     return wordResult
 
-if __name__ == "__main__":
-	userInput = input("Ingrese una oracion: ")
-	print(reverse(userInput))
+
+if __name__ == '__main__':
+    userInput = input('Ingrese una oracion: ')
+    print reverse(userInput)
